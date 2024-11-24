5b86fc7511853650769d1bbdb95977a13c4b9af4
milan-krocil <milan.krocil@email.cz>
2024-11-24 22:06:09 +0100

    vytvorena finalni verze

--- Projekt_1_Textovy analyzator_final.py
+++ Projekt_1_Textovy analyzator_final.py
@@ -36,8 +36,6 @@ author: Milan Kročil
 email: milan.krocil@email.cz
 '''
 oddelovac = ('-' * 79)
-
-print(hlavicka, oddelovac)
 jmeno = input('Zadej prihlasovaci jmeno: ')
 heslo = input('Zadej heslo: ')
 registrovani_uzivatele = {
@@ -47,81 +45,41 @@ registrovani_uzivatele = {
     'liz':'pass123'
     }
 
+print(hlavicka, oddelovac)
 print('username:', jmeno)
 print('password:', heslo)
-#print(oddelovac)
+
          
 if not jmeno in registrovani_uzivatele or registrovani_uzivatele [jmeno] != heslo:
-    #print('username:', jmeno)
-    #print('password:', heslo)
-    print('unregistred user, terminating the program..')
-    exit()
+  print('unregistred user, terminating the program..')
+  exit()
 else:
-    print(oddelovac)
-    print('Welcome to the app, ', jmeno) 
-    print('We have 3 texts to be analyzed')
-    print(oddelovac)
+  print(oddelovac)
+  print('Welcome to the app, ', jmeno) 
+  print('We have 3 texts to be analyzed')
+  print(oddelovac)
 
-#print('username:', jmeno)
-#print('password:', heslo)
-#print(oddelovac)
-
-#text_1 = TEXTS [0]
-#text_2 = TEXTS [1]
-#text_3 = TEXTS [2]
-#print(text_1) 
-#print(text_2) 
-#print(text_3) 
-######################################################################## 
-# VYBER TEXTU a jeho osetreni
-########################################################################
 vyber_textu = input('Enter a number btw. 1 and 3 to select: ')
 print(oddelovac)
 limit=3
-if not vyber_textu.isnumeric():                                                  # kontrola, ze se jedna o cislo
+if not vyber_textu.isnumeric():                                                 
   print('Nebylo zadano cislo textu. Program ukoncuji...')
   exit()
-if not int(vyber_textu) in range(1,limit+1):                                     # kontrola, ze je cislo ve spravnem rozsahu
+if not int(vyber_textu) in range(1,limit+1):                                     
   print('Nebyl vybran spravne cislo textu. Program ukoncuji...')
   exit()
 
-######################################################################
-# VYBRAN SPRAVNY TEXT k vybrane volbe
-######################################################################
 texty = {
    '1' : TEXTS [0],
    '2' : TEXTS [1],
    '3' : TEXTS [2]
 }
 vybrany_text = texty [vyber_textu]
-#print(vybrany_text)
 
-#####################################################################
-# UPRAVA/OCISTENI TEXT = vymazat pecialni znaky jako tecky, carky, dvojtecky..
-#####################################################################
 osetreny_text = []
 for ocisteni in vybrany_text.split():
    ocistene_slovo = ocisteni.strip('.,!-:')
    osetreny_text.append(ocistene_slovo)
-#print(osetreny_text) 
-
-###################################################################
-# VYPOCET POCET SLOV
-###################################################################
-
-pocet_slov = len(osetreny_text)
-#print('There are ', pocet_slov, 'words in he selected text.')
-
-list_pocet_cisel = []
-for cisla in osetreny_text:
-  if cisla.isnumeric():
-    list_pocet_cisel.append(cisla)
-
-#################################################################
-# VYPOCTY CETNOSTI SLOV (malymi, velkymi, zacinajici velkym) , CISEL a JEJICH SOUCTU
-
-pocet_cisel = len(list_pocet_cisel)
-#print('Celkovy pocet cisel v textu je: ', pocet_cisel)
 
 list_pocet_cisel = []
 list_pocet_slov_zacinajicich = []
@@ -141,43 +99,30 @@ for cisla in osetreny_text:
     list_pocet_slov_zacinajicich.append(cisla) 
  
 pocet_cisel = len(list_pocet_cisel)
-pocet_slov_zacinajici_velkymi_pismeny = len(list_pocet_slov_velkymi_pismeny)
-pocet_slov_zacinajici_malymi_pismeny = len(list_pocet_slov_malymi_pismeny)
-pocet_slov_zacinajicich_velkyma = len(list_pocet_slov_zacinajicich) 
+uppercase = len(list_pocet_slov_velkymi_pismeny)
+lowercase = len(list_pocet_slov_malymi_pismeny)
+titlecase = len(list_pocet_slov_zacinajicich) 
 celkovy_pocet_slov = len(osetreny_text)
 
-#print(osetreny_text)
 print('There are ', celkovy_pocet_slov, 'words in the selected text.')
-print('There are ', pocet_slov_zacinajicich_velkyma, 'titlecase words.')
-print('There are ', pocet_slov_zacinajici_velkymi_pismeny, 'uppercase words.')
-print('There are ', pocet_slov_zacinajici_malymi_pismeny, 'lowercase words.')
-#print('There are ', pocet_slov_zacinajicich_velkyma, 'titlecase words.')
-#print('Celkovy pocet slov je: ', celkovy_pocet_slov)
+print('There are ', titlecase, 'titlecase words.')
+print('There are ', uppercase, 'uppercase words.')
+print('There are ', lowercase, 'lowercase words.')
 print('There are ', pocet_cisel, 'numeric strings.') 
 print('The sum of all the numbers ', sum_cisel)
 print(oddelovac)
 
-########################################################################
-# SLOUPCOVY GRAF
-########################################################################
-from collections import Counter
-
-serazeny_seznam = sorted(osetreny_text, key = len)
-#print(serazeny_seznam)
-
-#delka = {}.fromkeys(serazeny_seznam,0)
 pridana_delka = []
-#print(delka)
-
+serazeny_seznam = sorted(osetreny_text, key = len)
 for delka_hodnoty in serazeny_seznam:
   pridana_delka.append(len(delka_hodnoty))
-#print(pridana_delka)
+
+from collections import Counter
 
 cetnost = (Counter(pridana_delka))
-#print(cetnost.most_common)
-list_cetnosti = cetnost.most_common()
 print(f"{'LEN':3}|{'OCCURENCES': ^18}|{'NR.'}")
 print(oddelovac)
 for key, value in cetnost.items():
    print(f"{key:3}|{'*' * int(value):18}|{value}", sep="\n")
-#print(type(cetnost))
+
+
